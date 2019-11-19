ae38760e6cd26a40cbaf0e11cd72dcc9e61f7e76
rafallmind <julian.aubertlyc@gmail.com>
2019-11-19 11:31:38 +0100

    splash screen

---
+++ app/src/main/java/com/example/volta/SplashScreen.java
@@ -0,0 +1,26 @@
+package com.example.volta;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends Activity {
+
+    private static int SPLASH_TIME_OUT = 1000;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
