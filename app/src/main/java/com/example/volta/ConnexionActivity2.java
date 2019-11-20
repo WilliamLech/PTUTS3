31ba1b37d329ff219e6621e555b7c2c88c5c1b4d
i181668 <gabrieledde@hotmail.com>
2019-11-20 16:40:48 +0100

    derniere version, design ok, inscription a faire

---
+++ app/src/main/java/com/example/volta/ConnexionActivity2.java
@@ -0,0 +1,14 @@
+package com.example.volta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConnexionActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connexion2);
+    }
+}
