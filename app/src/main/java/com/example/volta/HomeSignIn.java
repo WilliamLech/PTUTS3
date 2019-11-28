94c5363169ae267dc15f3c458136837a69915981
gabrieledde <gabrieledde@hotmail.com>
2019-11-28 13:19:08 +0100

    Last version, bug corrigé

--- app/src/main/java/com/example/volta/HomeSignIn.java
+++ app/src/main/java/com/example/volta/HomeSignIn.java
@@ -5,10 +5,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class HomeSignIn extends Activity {
 
     private Button button;
+    private TextView register;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,10 +18,21 @@ public class HomeSignIn extends Activity {
 
         button = findViewById(R.id.button);
 
+        register = findViewById(R.id.register);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(HomeSignIn.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(HomeSignIn.this, ConnexionActivity2.class);
                 startActivity(i);
             }
         });
